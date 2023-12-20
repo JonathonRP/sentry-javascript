b80439cd1fdfc5d55176319654afc7e48d17f641
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-20 18:01:36 +0100

    test(e2e): Add tests to SvelteKit 1.x E2E test app

--- dev-packages/e2e-tests/test-applications/sveltekit/src/hooks.server.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit/src/hooks.server.ts
@@ -1,17 +1,16 @@
-import { env } from '$env/dynamic/private';
-import * as Sentry from '@sentry/sveltekit';
+import { env } from "$env/dynamic/private";
+import * as Sentry from "@sentry/sveltekit";
 
 Sentry.init({
-  environment: 'qa', // dynamic sampling bias to keep transactions
+  environment: "qa", // dynamic sampling bias to keep transactions
   dsn: env.E2E_TEST_DSN,
   debug: true,
   tunnel: `http://localhost:3031/`, // proxy server
   tracesSampleRate: 1.0,
 });
 
-const myErrorHandler = ({ error, event }: any) => {
-  console.error('An error occurred on the server side:', error, event);
-};
+// not logging anything to console to avoid noise in the test output
+const myErrorHandler = ({ error, event }: any) => {};
 
 export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
 
